8a6b63b3c1f3bef66c1ca0b378a6eeba12fa3805
dharmayudho <sudarmayudhoprayitno@gmail.com>
2026-06-10 11:56:55 +0700

    fitur login, masih ada perlu tambahan lagi

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,33 @@
 import { Controller, Body, Get, Post, Render, Redirect, Req } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Get('login')
+  @Render('auth/login')
+  loginPage() {
+    return {};
+  }
+
+  @Post('login')
+  @Redirect('/kategori')
+  login(@Body() body: any, @Req() req: any) {
+    const email = 'admin@gmail.com';
+    const password = 'admin123';
+
+    if (body.email === email && body.password === password) {
+      req.session.user = {
+        email,
+      };
+    }
+
+    return {};
+  }
+
+  @Get('logout')
+  @Render('/auth/login')
+  logout(@Req() req: any) {
+    req.session.destroy();
+
+    return {};
+  }
+}
